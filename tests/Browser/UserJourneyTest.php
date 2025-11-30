604a9d55ef3378c04e584cc6b7add1da34f3722b
nannnmuzaki <162042041+nannnmuzaki@users.noreply.github.com>
2025-12-01 04:19:41 +0700

    feat: add browser test for user create new pengaduan flow

---
+++ tests/Browser/UserJourneyTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Database\Seeders\DatabaseSeeder;
+
+
+beforeEach(function () {
+    $this->seed(DatabaseSeeder::class);
+});
+
+test('User Journey buat pengaduan baru', function () {
+    $user = User::where('email', 'rina.marlina@gmail.com')->first();
+
+    visit('/')
+        ->assertSee('Platform pengaduan masyarakat')
+        ->assertDontSee('Pengaduan Saya')
+        ->navigate('/buat-pengaduan')
+        ->assertPathIs('/main/login')
+        ->type('[wire\:model="data.email"]', $user->email)
+        ->type('[wire\:model="data.password"]', 'password')
+        ->submit()
+        ->assertPathIs('/')
+        ->assertSee('Pengaduan Saya')
+        ->navigate('/buat-pengaduan')
+        ->type('[wire\:model="nama_pengadu"]', 'John Pork')
+        ->type('[wire\:model="email_pengadu"]', 'john.doe@example.com')
+        ->type('[wire\:model="telepon_pengadu"]', '081234567890')
+        ->type('[wire\:model="judul"]', 'Sampah Menumpuk di Jalan Raya')
+        ->select('[wire\:model="jenis_pengaduan_id"]', '3')
+        ->select('[wire\:model="kategori_pengaduan_id"]', '5')
+        ->select('[wire\:model="opd_id"]', '4')
+        ->type('[wire\:model="isi"]', 'Saya ingin melaporkan bahwa sampah menumpuk di jalan raya dekat rumah saya dan belum diangkut selama seminggu terakhir.')
+        ->pressAndWaitFor('Kirim Pengaduan', 2)
+        ->navigate('/pengaduan-saya')
+        ->assertSee('Sampah Menumpuk di Jalan Raya');
+});
