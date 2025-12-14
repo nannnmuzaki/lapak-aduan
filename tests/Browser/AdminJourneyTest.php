8288828f972bbe5be1b1583982b5e80da2a46e06
nannnmuzaki <162042041+nannnmuzaki@users.noreply.github.com>
2025-12-14 21:07:52 +0700

    feat: add more steps for more realistic test

--- tests/Browser/AdminJourneyTest.php
+++ tests/Browser/AdminJourneyTest.php
@@ -19,12 +19,15 @@ test('Admin journey balas pengaduan', function () {
         ->assertPathIs('/main')
         ->assertSee('Dashboard')
         ->navigate('/main/pengaduans')
-        ->assertSee('Sampah menumpuk di TPS')
-        ->pressAndWaitFor('Verifikasi & Balas', 1)
-        ->assertSee('Verifikasi dan Balas Pengaduan')
+        ->assertSee('Daftar Pengaduan')
+        ->pressAndWaitFor('View', 1) // buka detail pengaduan
+        ->press('[title="Close"]') // tutup modal
+        ->pressAndWaitFor('Verifikasi & Balas', 1) // buka modal form verifikasi dan balas
+        ->assertSee('Verifikasi dan Balas Pengaduan') 
+        ->press('[id="mountedActionSchema0.is_verified"]') // centang Verifikasi Pengaduan
         ->type('[role="textbox"]', 'Terima kasih atas laporannya. Kami akan segera menindaklanjuti masalah ini.')
-        ->press('Verifikasi dan Balas Pengaduan')
-        ->select('.fi-modal [wire\:model*="status_respon"]', 'dalam_proses')
-        ->press('Simpan')
-        ->assertSee('Pengaduan berhasil diperbarui');
+        ->press('Verifikasi dan Balas Pengaduan') // press random agar keluar dari fokus textbox rich text editor
+        ->select('.fi-modal [wire\:model*="status_respon"]', 'telah_direspon') // pilih status respon sebagai telah direspon
+        ->press('Simpan') // simpan respon
+        ->assertSee('Pengaduan berhasil diperbarui'); // verifikasi berhasil
 });
